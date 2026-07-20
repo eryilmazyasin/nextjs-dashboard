f72dd1e2605290327bb28186a39d1ebf4517dac0
Yasin Eryılmaz <yasin.eryilmaz@metus.com>
2026-07-20 16:11:22 +0300

    Fetching data from database

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,8 +1,9 @@
-import postgres from "postgres";
+import postgres from 'postgres';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 async function listInvoices() {
+  // Database sorgusu: Bu sorgu, invoices tablosundaki amount ve customers tablosundaki name sütunlarını seçer. Ayrıca, invoices tablosu ile customers tablosunu customer_id sütunu üzerinden birleştirir ve amount değeri 666 olan kayıtları filtreler.
   const data = await sql`
     SELECT invoices.amount, customers.name
     FROM invoices
